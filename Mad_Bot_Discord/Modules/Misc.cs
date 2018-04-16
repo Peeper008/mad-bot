0f4a1c388c545883f8370c82be12ea524ebfad22
Hunter Green <huntergreen008@gmail.com>
2018-04-16 15:00:40 -0700

    Added CommandHandler.cs, Config.cs, Utilities.cs and Misc.cs
    Added temporary Echo command in Misc.cs
    
    Base of the bot is finished; now time to implement the commands.

---
+++ Mad_Bot_Discord/Modules/Misc.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_Bot_Discord.Modules
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder] string message)
+        {
+            var embed = new EmbedBuilder();
+
+            embed.WithTitle("Message by " + Context.User.Username)
+                .WithDescription(message)
+                .WithColor(0, 255, 0);
+
+            await Context.Channel.SendMessageAsync("", false, embed);
+        }
+    }
+}
