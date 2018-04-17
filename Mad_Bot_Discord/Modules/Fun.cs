c15b762d8453b2fce8c68b82546382e9e80d07b8
Hunter Green <huntergreen008@gmail.com>
2018-04-17 14:49:44 -0700

    Added roll command.

--- Mad_Bot_Discord/Modules/Fun.cs
+++ Mad_Bot_Discord/Modules/Fun.cs
@@ -36,7 +36,7 @@ namespace Mad_Bot_Discord.Modules
 
         }
 
-        [Command("8ball")]
+        [Command("8Ball")]
         public async Task EightBall([Remainder] string question = "[No Question]")
         {
             string json = File.ReadAllText("SystemLang/8ballAnswers.json");
@@ -55,6 +55,102 @@ namespace Mad_Bot_Discord.Modules
                 {
                     x.IconUrl = Context.User.GetAvatarUrl();
                     x.Text = un + Context.User.Discriminator + " at " + Context.Message.Timestamp;
+                });
+
+            await Context.Channel.SendMessageAsync("", embed: embed);
+        }
+
+        [Command("Roll")]
+        public async Task Roll([Remainder] string minmax = "1|6")
+        {
+            string[] ns = minmax.Split(new char[] { ' ', '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!int.TryParse(ns[0], out int n1))
+            {
+                var embed2 = new EmbedBuilder();
+                embed2.WithTitle("Failed Roll")
+                    .WithColor(Utilities.GetColor())
+                    .WithFooter(x =>
+                    {
+                        x.Text = $"{Context.User.Username}#{Context.User.Discriminator} at {Context.Message.Timestamp}";
+                        x.IconUrl = Context.User.GetAvatarUrl();
+                    })
+                    .WithDescription($"Failed to parse {ns[0]}. Roll only supports integers, you can separate them using a space or the '|' key. " +
+                    $"The max integer allowed is 2147483646.");
+
+                await Context.Channel.SendMessageAsync("", embed: embed2);
+                return;
+            }
+            if (!int.TryParse(ns[1], out int n2))
+            {
+                var embed2 = new EmbedBuilder();
+
+                embed2.WithTitle("Failed Roll")
+                    .WithColor(Utilities.GetColor())
+                    .WithFooter(x =>
+                    {
+                        x.Text = $"{Context.User.Username}#{Context.User.Discriminator} at {Context.Message.Timestamp}";
+                        x.IconUrl = Context.User.GetAvatarUrl();
+                    })
+                    .WithDescription($"Failed to parse {ns[1]}. Roll only supports integers, you can separate them using a space or the '|' key. " +
+                    $"The max integer allowed is 2147483646.");
+
+                await Context.Channel.SendMessageAsync("", embed: embed2);
+                return;
+            }
+
+            if (n2 < n1)
+            {
+                var embed3 = new EmbedBuilder();
+                embed3.WithTitle("Failed Roll")
+                    .WithColor(Utilities.GetColor())
+                    .WithDescription("The max number must be larger than the min number.")
+                    .WithFooter(x =>
+                    {
+                        x.Text = Context.User.Username + "#" + Context.User.Discriminator + " at " + Context.Message.Timestamp;
+                        x.IconUrl = Context.User.GetAvatarUrl();
+                    });
+
+                await Context.Channel.SendMessageAsync("", embed: embed3);
+                return;
+            }
+            else
+                n2++;
+
+            int selection;
+
+            try
+            {
+                selection = r.Next(n1, n2);
+                
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                var embed3 = new EmbedBuilder();
+                embed3.WithTitle("Failed Roll")
+                    .WithColor(Utilities.GetColor())
+                    .WithDescription("The max number must be larger than the min number.")
+                    .WithFooter(x =>
+                    {
+                        x.Text = Context.User.Username + "#" + Context.User.Discriminator + " at " + Context.Message.Timestamp;
+                        x.IconUrl = Context.User.GetAvatarUrl();
+                    });
+
+                await Context.Channel.SendMessageAsync("", embed: embed3);
+
+                return;
+            }
+            
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Roll for " + Context.User.Username)
+                .WithColor(Utilities.GetColor())
+                .WithDescription("Your two numbers were... " + ns[0] + " and " + ns[1])
+                .AddInlineField("**New Number:**", selection)
+                .WithFooter(x =>
+                {
+                    x.IconUrl = Context.User.GetAvatarUrl();
+                    x.Text = Context.User.Username + "#" + Context.User.Discriminator + " at " + Context.Message.Timestamp;
                 });
 
             await Context.Channel.SendMessageAsync("", embed: embed);
